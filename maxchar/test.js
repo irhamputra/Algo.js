dfbc8df6b3966e285eb1e4e20b95f8d63fc64552
irhamputra <irhamputraprasetyo@gmail.com>
2018-01-10 19:38:18 +0100

    number in a string

--- maxchar/test.js
+++ maxchar/test.js
@@ -10,5 +10,5 @@ test('Finds the most frequently used char', () => {
 });
 
 test('Works with numbers in the string', () => {
-   expect(maxChar('ab1c1d1e1f11gh11')).toEqual('1');
+   expect(maxChar('ab1c1d1e1f11g1h11')).toEqual('1');
 });
