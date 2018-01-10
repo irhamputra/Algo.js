ad57009af71a55b7e524ffffabbbc7febe9e86c2
irhamputra <irhamputraprasetyo@gmail.com>
2018-01-10 16:52:15 +0100

    add palindrome questions

---
+++ palindrome/index.js
@@ -0,0 +1,11 @@
+// -- Petunjuk Soal
+// String yang sudah diberikan, harus return true kalau string adalah palindrome
+// atau false jika bukan palindrome. Palindromes adalah string yang jika direverse
+// akan memiliki form string yang sama. Berlaku juga untuk spasi
+// -- Contoh
+// palindrome('abba') === true karena 'abba' direverse akan menjadi 'abba'
+// palindrome('abcdef') === false karena 'abcdef' direverse akan menjadi 'fedcba'
+
+function palindrome(str) {}
+
+module.exports = palindrome;
