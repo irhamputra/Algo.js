088067709c547a1731acce92075dabf7f4b86174
irhamputra <irhamputraprasetyo@gmail.com>
2018-01-10 19:37:38 +0100

    number is also works in a string

--- maxchar/test.js
+++ maxchar/test.js
@@ -9,3 +9,6 @@ test('Finds the most frequently used char', () => {
    expect(maxChar('abcdefghijkkkkkkkk')).toEqual('k');
 });
 
+test('Works with numbers in the string', () => {
+   expect(maxChar('ab1c1d1e1f11gh11')).toEqual('1');
+});
