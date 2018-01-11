0cb4b8e293053c24f517348ee48876d48b4a37d5
irhamputra <irhamputraprasetyo@gmail.com>
2018-01-11 16:37:55 +0100

    Add chunk array testing

--- chunk/test.js
+++ chunk/test.js
@@ -12,3 +12,23 @@ test('chunk divides an array of 10 elements with chunk size 2', () => {
 });
 
 // TODO: add more testing down below
+test('chunk divides an array of 3 elements with chunk size 1', () => {
+    const arr = [1, 2, 3];
+    const chunked = chunk(arr, 1);
+
+    expect(chunked).toEqual([[1], [2], [3]]);
+});
+
+test('chunk divides an array of 5 elements with chunk size 3', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const chunked = chunk(arr, 3);
+
+    expect(chunked).toEqual([[1, 2, 3], [4, 5]]);
+});
+
+test('chunk divides an array of 13 elements with chunk size 5', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    const chunked = chunk(arr, 5);
+
+    expect(chunked).toEqual([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13]]);
+});
