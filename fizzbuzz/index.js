b0bc97409de350c20211457ce3804f4057d3d07e
irhamputra <irhamputraprasetyo@gmail.com>
2018-01-11 07:38:56 +0100

    add fizzbuzz question

---
+++ fizzbuzz/index.js
@@ -0,0 +1,16 @@
+// -- Petunjuk Soal
+// Tulis sebuah program yang berisi console log untuk mencatat angka dari 1 sampai n.
+// Tapi untuk kelipatan 3 harus print kata "fizz" bukan angka,
+// dan untuk kelipatan 5 harus print kata "buzz".
+// Untuk bilangan kelipatan angka keduanya harus print "fizzBuzz"
+// -- Contoh
+// fizzBuzz(5);
+// 1
+// 2
+// fizz
+// 4
+// buzz
+
+function fizzBuzz(n) {}
+
+module.exports = fizzBuzz();
