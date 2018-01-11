ab76d3d00bd0adbe427b99cd51ddc19d358bebf6
irhamputra <irhamputraprasetyo@gmail.com>
2018-01-11 09:01:05 +0100

    merging fibonacci question

--- chunk/test.js
+++ chunk/test.js
@@ -1,3 +1,14 @@
-/**
- * Created by MacbookPro on 1/11/18.
- */
+const chunk = require('./index');
+
+test('function chunk exists', () => {
+    expect(typeof chunk).toEqual('function');
+});
+
+test('chunk divides an array of 10 elements with chunk size 2', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const chunked = chunk(arr, 2);
+
+    expect(chunked).toEqual([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]);
+});
+
+// TODO: add more testing down below
